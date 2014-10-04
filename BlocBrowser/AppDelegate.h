280bbb3c42c30e2e28fcde359e1cb1b1ab9ecb60
John Adapon <john.adapon@gmail.com>
2014-10-04 16:22:59 -0700

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by John Patrick Adapon on 10/4/14.
+//  Copyright (c) 2014 John Adapon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
